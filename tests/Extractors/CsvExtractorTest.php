4a3c0ff6736cd71f0f794d228bcda78cfd01fd4d
Eduardo Bizarro <edbizarro@gmail.com>
2018-06-26 15:10:13 -0300

    improv tests

--- tests/Extractors/CsvExtractorTest.php
+++ tests/Extractors/CsvExtractorTest.php
@@ -7,11 +7,35 @@ use PHPUnit\Framework\TestCase;
 
 class CsvExtractorTest extends TestCase
 {
-    public function test_can_extract_a_csv()
+   /** @test */
+    public function it_can_read_a_csv()
+    {
+        $result = (new CsvExtractor())->extract(__DIR__.'/../Extractors/stubs/csv-extracsv-stub.csv');
+        $this->assertInstanceOf(\Generator::class, $result);
+        $this->assertCount(1, $result);
+    }
+
+    /** @test */
+    public function it_can_extract_a_csv()
     {
         $result = (new CsvExtractor())->extract(__DIR__.'/../Extractors/stubs/csv-extracsv-stub.csv');
 
-        $this->assertInstanceOf(\Generator::class, $result);
-        $this->assertCount(1, $result);
+        foreach ($result as $value) {
+          $this->assertEquals('John Doe', $value['Name']);
+          $this->assertEquals('São Paulo', $value['State']);
+          $this->assertEquals('Blue', $value['Color']);
+        }
+    }
+
+    /** @test */
+    public function it_can_extract_a_csv_with_different_delimiter()
+    {
+        $result = (new CsvExtractor())->extract(__DIR__.'/../Extractors/stubs/csv-delimiter-stub.csv', ";");
+
+        foreach ($result as $value) {
+          $this->assertEquals('John Doe', $value['Name']);
+          $this->assertEquals('São Paulo', $value['State']);
+          $this->assertEquals('Blue', $value['Color']);
+        }
     }
 }
